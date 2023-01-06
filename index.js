a072a7e0432024ab6fc8cfc7f9c4ecabcbd823b2
Vinicius Dutra <vfdutra@gmail.com>
2023-01-06 12:51:14 -0300

    Uns arquivos ai

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-var http = require('http');
-const fs = require('fs');
 const Libxml = require('node-libxml');
-
 let libxml = new Libxml();
-let xmlIsWellformed = libxml.loadXml('poema.xml');
-libxml.loadDtds(['poema.dtd']);
-let xmsIsValid = libxml.validateAgainstDtds();
+libxml.loadXml('arquivo.xml');
 
-http.createServer(function (req, res) {
-    console.log(xmsIsValid);
-    console.log(libxml.validationDtdErrors);
-}).listen(8080);
+libxml.loadDtds(['arquivo.dtd']);
+let xmsIsValidBasedOfDTD = libxml.validateAgainstDtds();
 
+libxml.loadSchemas(['arquivo.xsd']);
+let xmsIsValidBasedOfXSD = libxml.validateAgainstSchemas();
+
+// console.log(xmsIsValidBasedOfDTD);
+// If the DTD validation generates errors, they are stored in the validationDtdErrors property
+// console.log(libxml.validationDtdErrors);
+
+// console.log(xmsIsValidBasedOfXSD);
+// If the XSD validation generates errors, they are stored in the validationSchemaErrors property
+// console.log(libxml.validationSchemaErrors);
+
+var xpath = require('xpath'), dom = require('xmldom').DOMParser;
+var fs = require('fs');
+var xml = fs.readFileSync('arquivo.xml', 'utf8');
+var doc = new dom().parseFromString(xml, 'text/xml');
+
+//Get from the XML all the generos from the arquivo.xml
+let nodes = xpath.select('//topicMap/topic/instanceOf/topicRef[@href="#Genero"]/parent::*/following-sibling::baseName/baseNameString', doc);
+
+//Get the values of the baseNameString without repetition
+let genres = nodes.map(node => node.firstChild.data.trim());
+
+let generos = genres.map((genre) => {
+    if(genre.includes('/')){
+        genre = genre.replace(/\s\/\s/g, '/');        
+    }
+
+    return genre;
+})
+
+let array = [...new Set(generos)]
+
+console.log(array);
