66e6f4ea4e24342fa2a40d86e61f2723f25d4659
Vinicius Dutra <vfdutra@gmail.com>
2023-01-10 17:57:40 -0300

    Todas as consultas

--- index.js
+++ index.js
@@ -22,13 +22,11 @@ var fs = require('fs');
 var xml = fs.readFileSync('arquivo.xml', 'utf8');
 var doc = new dom().parseFromString(xml, 'text/xml');
 
-//Get from the XML all the generos
+//Find all the genres
 let nodes = xpath.select('//topicMap/topic/instanceOf/topicRef[@href="#Genero"]/parent::*/following-sibling::baseName/baseNameString', doc);
 
-//Get only the generos data of the nodes
 let genres = nodes.map(node => node.firstChild.data.trim());
 
-//Find the occurences inside the code there have a slash and remove its spaces
 let generos = genres.map((genre) => {
     if(genre.includes('/')){
         genre = genre.replace(/\s\/\s/g, '/');        
@@ -37,29 +35,9 @@ let generos = genres.map((genre) => {
     return genre;
 })
 
-
-//Remove the repetition of the genres
 let array = [...new Set(generos)]
 
-//Find all the movies from 2000's
-let movies_2000 = xpath.select('//association/member/topicRef[@href="#id_2000"]/../preceding-sibling::member/topicRef/@href', doc);
-
-let movies_2 = movies_2000.map((movie) => movie.nodeValue.replace('#', '').replace('-', ' ').replace("id", "").replace("_", ""));
-
-//Find all the movies with 'special' on the synopsis
-let special_movies = xpath.select("//topic/occurrence/scope/topicRef[@href='#sinopse']/following::resourceData[contains(text(), 'especial')]/parent::*/preceding-sibling::occurrence/scope/topicRef[@href='#ingles']/../following-sibling::resourceData", doc);
-
-let spe_movies = special_movies.map((movie) => movie.firstChild.data);
-
-//Get the site from the thriller movies
-let thriller_movies = xpath.select('//association/member/topicRef[@href="#thriller"]/../preceding-sibling::member/topicRef/@href', doc);
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  switch(req.url){
-    case '/generos': 
-        var html = `
+var questao_1 = `
         <!DOCTYPE html>
         <html>
           <head>
@@ -71,19 +49,29 @@ const server = http.createServer((req, res) => {
             <ul>
         `;
         array.forEach((genre) => {
-            html += `<li>${genre}</li>`;
+            questao_1 += `<li>${genre}</li>`;
         });
 
-        html += `
+        questao_1 += `
             </ul>
           </body>
         </html>
         `;
-        res.end(html);
-        break;
 
-    case '/filmes-2000':  
-        var html = `
+fs.writeFile('consultas/questao01.html', questao_1, (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('File created successfully!')
+});
+
+//Find all the movies from 2000's
+let movies_2000 = xpath.select('//association/member/topicRef[@href="#id_2000"]/../preceding-sibling::member/topicRef/@href', doc);
+
+let movies = movies_2000.map((movie) => movie.nodeValue.replace('#', '').replace('-', ' ').replace("id", "").replace("_", ""));
+
+var questao_2 = `
         <!DOCTYPE html>
         <html>
           <head>
@@ -94,44 +82,62 @@ const server = http.createServer((req, res) => {
             <h1>Filmes do ano de 2000</h1>
             <ul>
         `;
-        movies_2.forEach((movie) => {
-            html += `<li>${movie}</li>`;
+        movies.forEach((movie) => {
+            questao_2 += `<li>${movie}</li>`;
         });
 
-        html += `
+        questao_2 += `
             </ul>
           </body>
         </html>
         `;
-        res.end(html);
-        break;
 
-    case '/filmes-especiais':
-      var html = `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="UTF-8" />
-          <title>Filmes Especiais</title>
-        </head>
-        <body>
-          <h1>Filmes Especiais</h1>
-          <ul>
-      `;
-      spe_movies.forEach((movie) => {
-          html += `<li>${movie}</li>`;
-      });
+fs.writeFile('consultas/questao02.html', questao_2, (err) => {
+  if (err) {
+      console.log(err);
+      return;
+  }
+  console.log('File created successfully!')
+});
 
-      html += `
-          </ul>
-        </body>
-      </html>
-      `;
-      res.end(html);
-      break;
+// Find all the movies with 'special' on the synopsis
+let special_movies = xpath.select("//topic/occurrence/scope/topicRef[@href='#sinopse']/following::resourceData[contains(text(), 'especial')]/parent::*/preceding-sibling::occurrence/scope/topicRef[@href='#ingles']/../following-sibling::resourceData", doc);
 
-    case '/filmes-thriller': {
-        var html = `
+let spe_movies = special_movies.map((movie) => movie.firstChild.data);
+
+var questao_3 = `
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="UTF-8" />
+            <title>Filmes Especiais</title>
+          </head>
+          <body>
+            <h1>Filmes Especiais</h1>
+            <ul>
+        `;
+        spe_movies.forEach((movie) => {
+            questao_3 += `<li>${movie}</li>`;
+        });
+
+        questao_3 += `
+            </ul>
+          </body>
+        </html>
+        `;
+
+fs.writeFile('consultas/questao03.html', questao_3, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('File created successfully!')
+});
+        
+// Get the site from the thriller movies
+let thriller_movies = xpath.select('//association/member/topicRef[@href="#thriller"]/../preceding-sibling::member/topicRef/@href', doc);
+
+var questao_4 = `
         <!DOCTYPE html>
         <html>
           <head>
@@ -149,23 +155,123 @@ const server = http.createServer((req, res) => {
           
           let movie_site = movie_site_node.map(movie_node => movie_node.nodeValue);
 
-          html += `<li>${movie_site}</li>`;       
-      });
-        html += `
-        </ul>
-        </body>
-      </html>
-      `;
-      res.end(html);
-      break;
-    }
+          questao_4 += `<li>${movie_site}</li>`;       
+        });
 
-    default:  
-        res.end('Hello World');
-        break;
+          questao_4 += `
+          </ul>
+          </body>
+        </html>
+        `;
+
+fs.writeFile('consultas/questao04.html', questao_4, (err) => {
+  if (err) {
+    console.log(err);
+    return;
   }
+  console.log('File created successfully!')
 });
 
-server.listen(8000, () => {
-  console.log('Server listening on port 8000');
+//Get the number of movies with more than 3 elenco-apoio
+let $topics = xpath.select('//topic', doc); 
+
+let $topic = $topics.map((topic) => {
+  let $filme_id = topic.getAttribute('id');
+  
+  let $elencoApoio = xpath.select(`count(//topic[@id="${$filme_id}"]/occurrence/scope/topicRef[@href="#elencoApoio"])`, doc);  
+
+  return $elencoApoio;
+}).filter(n => n > 3).length;
+
+var questao_5 = `
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="UTF-8" />
+            <title>Filmes Elenco Apoio</title>
+          </head>
+          <body>
+            <h1>Filmes Com Elenco Apoio Maior que 3</h1>
+            <p>${$topic}</p>
+          </body>
+        </html>
+        `;
+
+fs.writeFile('consultas/questao05.html', questao_5, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('File created successfully!')
+});
+
+// //Get the number of ID's there have the name of one of it actors on the sinopse
+let $associations = xpath.select('//association/instanceOf/topicRef[@href="#filme-elenco"]/../following-sibling::member[1]/topicRef/@href', doc);
+
+let $actors_names = $associations.map((association) => {
+  let $actors = xpath.select(`//association/member/topicRef[@href="${association.nodeValue}"]/../preceding-sibling::instanceOf/topicRef[@href="#filme-elenco"]/../following-sibling::member[2]/topicRef/@href`, doc)  
+
+  return $actors;
+});
+
+let $array = [];
+
+for(var i = 0; i < $actors_names.length; i++){
+  for(var j = 0; j < $actors_names[i].length; j++){
+    if($array.includes($actors_names[i][j].nodeValue)){
+      continue; 
+    }else{
+      $array.push($actors_names[i][j].nodeValue);      
+    }
+  }
+}
+
+let $array_2 = $array.map((actor) => {
+  let $actor = actor.replace('#', '').replace('-', ' ').replace("id", "").replace("_", "");
+  $actor = $actor.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  return $actor;
+});
+
+let $filmes_id = $array_2.map((actor) => {
+  let $sinopse = xpath.select(`//topic/occurrence/scope/topicRef[@href="#sinopse"]/following::resourceData[contains(text(), "${actor}")]/../../@id`, doc);
+  return $sinopse;
+});
+
+let $array3 = []
+
+for(var i = 0; i < $filmes_id.length; i++){
+  for(var j = 0; j < $filmes_id[i].length; j++){
+    if($array3.includes($filmes_id[i][j].nodeValue)){
+      continue;
+    }else{
+      $array3.push($filmes_id[i][j].nodeValue);
+    }
+  }
+}
+
+var questao_6 = `
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="UTF-8" />  
+            <title>Filmes Elenco Atores</title>
+          </head>
+          <body>
+            <h1>Filmes Elenco Atores>
+            <ul>
+        `;
+        $array3.map((movie) => {
+          questao_6 += `<li>${movie}</li>`;   
+        });
+          questao_6 += `          </ul>
+          </body>
+        </html>
+        `;
+
+fs.writeFile('consultas/questao06.html', questao_6, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('File created successfully!')
 });
